234b10a130d98c6842ae965c1f11088eaf7a371e
Nikesh S Raj <nikeshsraj@gmail.com>
2021-04-18 19:05:26 -0400

    Frontend Skeleton

---
+++ dineout/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        Dine Out
+      </header>
+    </div>
+  );
+}
+
+export default App;
