586450f463e5af2b712fe3fe0ad9d10f6e2945c6
Shoaib Sheikh <shoaibsheikh.48@gmail.com>
2021-04-27 18:48:26 -0400

    removed extra strapi

--- dineout/my-project/config/server.js
+++
@@ -1,9 +0,0 @@
-module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
-  admin: {
-    auth: {
-      secret: env('ADMIN_JWT_SECRET', '8f29f14861f77cd8baa0e0d7ebadad5a'),
-    },
-  },
-});
--- dineout/my-project/extensions/users-permissions/config/jwt.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-  jwtSecret: process.env.JWT_SECRET || '0d3f3ab8-bcfc-493e-998b-be6cbe3b363e'
-};
