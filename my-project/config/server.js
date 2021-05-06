8011536d688a99b89eb3a22cb5b3f35044cbb243
Shoaib Sheikh <shoaibsheikh.48@gmail.com>
2021-05-06 13:33:17 -0400

    deleted my-project

--- my-project/config/server.js
+++
@@ -1,9 +0,0 @@
-module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
-  admin: {
-    auth: {
-      secret: env('ADMIN_JWT_SECRET', 'e4bd4727094991e79733af02414e6a02'),
-    },
-  },
-});
--- my-project/extensions/users-permissions/config/jwt.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-  jwtSecret: process.env.JWT_SECRET || 'ffc03e3b-67e8-48d4-a44d-7e48cca9a88d'
-};
